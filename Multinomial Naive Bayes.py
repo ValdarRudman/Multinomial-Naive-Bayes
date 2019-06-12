7aa1450757829c4513e068cfdbdeebc88668525a
ValdarRudman <43271709+ValdarRudman@users.noreply.github.com>
2019-06-12 12:54:17 +0100

    Update Multinomial Naive Bayes.py
    Fixed calculating pos/neg/unknown value for words and the pos/neg/unknown for tweets. Rewrote a few small methods

--- Multinomial Naive Bayes.py
+++ Multinomial Naive Bayes.py
@@ -4,118 +4,64 @@
 #   Valdar Rudman
 #   R00081134
 
-import matplotlib.pyplot as plt
-
-plt.rcdefaults
-import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
-
-# Clean up the imported words
-def cleanUp(tweets):
-    stop_words = set(stopwords.words('english'))
-
-    filtered_tweets = []
-    filtered_tweet = ""
-
-    # Go through tweets one tweet at a time and
-    # split tweet to go through one word at a time
-    for tweet in tweets:
-        word_tokens = word_tokenize(tweet)
-
-        for w in word_tokens:
-            if w not in stop_words:
-                # Append all words on the current tweet to each other with a white
-                # space at the start of each word
-                filtered_tweet += " "
-                filtered_tweet += w
-
-        # Add the new tweet string to a list(List of tweets) and remove
-        # white space at the start of tweet
-        filtered_tweets.append(filtered_tweet.lstrip())
-    return filtered_tweets
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read a file in and split on the white space
 def readFile(source):
     return open(source).read().split()
 
-
 # Read a file in and split on new line
-def readFileTweets(source):
+def readTweetsFile(source):
     return open(source).read().lower().split("\n")
-
-
-# Counts the number of words in a set and increments the value in the dict by 1
-def no_of_words(list, dict):
-    for word in list:
-        dict[word] = dict[word] + 1
-    return dict
-
 
 # Gets the probability of the word. This can be the probability
 # of a word being positive or negative
-def prob_of_word(prob, fullDict):
+def prob_of_word(percentDict, fullDict):
     words = {}
 
-    for word in fullDict:
-        words[word] = (prob[word] / fullDict[word])
-
+    for word in percentDict:
+        words[word] = (percentDict[word] / fullDict[word])
     return words
 
+# Takes a list of words in and returns list without stopwords
+def removeStopWords(sentence):
 
-# Gets the probability of tweets being positive or negative
-def prob_of_new_tweets(tweets, wordPos, wordNeg):
-    posTweets, negTweets, unknownTweets = 0, 0, 0
+    stopWords = set(stopwords.words('english'))
+    words = word_tokenize(sentence)
+    wordsFiltered = []
 
-    # Going through tweets one tweet at a time
+    for w in words:
+        if w not in stopWords:
+            wordsFiltered.append(w)
+
+    return wordsFiltered
+
+# Working out if tweets are positive or negative
+def posNegTweets(tweets, wordsPos, wordsNeg):
+    posTweets, negTweets, uknownTweets = 0, 0, 0
     for tweet in tweets:
-
-        # Split tweet into words and go through words
         words = tweet.split()
-
-        # Count the positive words and negative words of
-        # current tweet being processed
-        posWords, negWords = 0, 0
-
+        posWords, negWords, uknownWord, count = 0, 0, 0, 1
         for word in words:
+            if word in wordsPos:
+                posWords += wordsPos[word]
+            if word in wordsNeg:
+                negWords += wordsNeg[word]
+            count += 1
+        posWords = posWords / count
+        negWords = negWords / count
 
-            # Checks if word is in positive and negative dictionaries
-            # If word in, gets the probability of the word and adds
-            # it to the posWords or negWords
-            # e.g word love: probability of positive 0.8
-            # probability of negative #0.2
-            # posWords += 0.8, negWords += 0.2
-            if word in wordPos:
-                # Originally I multiplied the probability
-                # posWords *= wordPos[word]
-                # But I found that sometimes the if check
-                # if wordPos[word] > 0:
-                # would not stop a word with value of 0
-                # so I end up with posWords = 0
-                # I just change the * to + as I don't need the overall
-                # percentage just the sum of pos and neg
-                # if wordPos[word] > 0:
-                posWords += wordPos[word]
-            if word in wordNeg:
-                # if wordNeg[word] > 0:
-                negWords += wordNeg[word]
-
-        # Compares posWords to negWords and vice versa
-        # if posWords value at the end is higher: positive tweet
-        # e.g posWords = 1.5 this not a percentage but a total of
-        # the percentage probability of the words
-        # negWords = 1.3. negWords less => positive tweet
-        if negWords > posWords:
-            negTweets += 1
-        elif posWords > negWords:
+        if posWords > negWords:
             posTweets += 1
+        elif negWords > posWords:
+            negTweets += 1
         else:
-            unknownTweets += 1
-
-    return (posTweets / (posTweets + negTweets + unknownTweets)) * 100
-
+            uknownTweets += 1
+    # Returns a list [percent of positive tweets in the batch, percent of negative tweets in the batch, percent of unkown tweets in the batch]
+    return [((posTweets / len(tweets)) * 100), ((negTweets / len(tweets)) * 100), ((uknownTweets / len(tweets)) * 100)]
 
 # Graph the before and after results of pre-processing for both negative and positive
 def graph(PositiveBeforePP, positiveAfterPP, negativeBeforePP, negativeAfterPP):
@@ -133,58 +79,70 @@ def graph(PositiveBeforePP, positiveAfterPP, negativeBeforePP, negativeAfterPP):
     plt.title("Tweets Accuracy")
     plt.show()
 
-
 def main():
-    posList = readFile("trainPos.txt")
-    negList = readFile("trainNeg.txt")
+    print("Reading in Training Files...")
+    posList = readFile("train\\trainPos.txt")
+    negList = readFile("train\\trainNeg.txt")
+
+    posList = [item.lower() for item in posList]
+    negList = [item.lower() for item in negList]
+
+    print("Removing stopwords from training files...")
+    # print(negList)
+    posList = removeStopWords(' '.join(posList))
+    negList = removeStopWords(' '.join(negList))
 
     # Getting unique words for positive and negative as well as getting a full set of them
-    uniquePosSet = set(posList)
-    uniqueNegSet = set(negList)
-    fullSet = uniquePosSet | uniqueNegSet
+    posSet = set(posList)
+    negSet = set(negList)
+    fullSet = posSet|negSet
 
+    print("Creating dictionaries...")
     # Creating dictionaries to use to keep count of how many times a word show up
-    posDict = dict.fromkeys(fullSet, 0)
-    negDict = dict.fromkeys(fullSet, 0)
+    posDict = dict.fromkeys(posSet, 0)
+    negDict = dict.fromkeys(negSet, 0)
     fullDict = dict.fromkeys(fullSet, 0)
 
-    # Count words for full dictionary
     for word in posList:
+        posDict[word] = posDict[word] + 1
         fullDict[word] = fullDict[word] + 1
 
     for word in negList:
+        negDict[word] = negDict[word] + 1
         fullDict[word] = fullDict[word] + 1
 
-    # Counting words for positive and negative dictionary
-    posDict = no_of_words(posList, posDict)
-    # print(posDict)
-    negDict = no_of_words(negList, negDict)
-    # print(negDict)
+    # print("Negative: ", negDict)
+    # print("Full: ", fullDict)
 
-    # Getting the probability that a word is positive or negative
+    print("Calculate words pos/neg value...")
     wordsPos = prob_of_word(posDict, fullDict)
-    # print(wordsPos)
     wordsNeg = prob_of_word(negDict, fullDict)
-    # print(wordsNeg)
 
-    # Reading in the test Positive file and cleaning up the result
-    posTweets = readFileTweets("testPos.txt")
-    # print(prob_of_new_tweets(posTweets, wordsPos, wordsNeg))
-    posTweetsCleanUp = cleanUp(readFileTweets("testPos.txt"))
-    # print(prob_of_new_tweets(posTweetsCleanUp, wordsPos, wordsNeg))
+    print("Reading in Pos Tweets and removing stopwords...")
+    posTweets = readTweetsFile("test\\testPos.txt")
+    posTweetsCleanedUp = []
+    for tweet in posTweets:
+        tweet.lower()
+        posTweetsCleanedUp.append(' '.join(removeStopWords(tweet)))
 
-    # Reading in the negative file and cleaning up the result
-    negTweets = readFileTweets("testNeg.txt")
-    # print(100 - prob_of_new_tweets(negTweets, wordsPos, wordsNeg))
-    negTweetsCleanUp = cleanUp(readFileTweets("testNeg.txt"))
-    # print(100 - prob_of_new_tweets(negTweetsCleanUp, wordsPos, wordsNeg))
+    print("Reading in Neg Tweets and removing stopwords...")
+    negTweets = readTweetsFile("test\\testNeg.txt")
+    negTweetsCleanedUp = []
+    for tweet in negTweets:
+        tweet.lower()
+        negTweetsCleanedUp.append(' '.join(removeStopWords(tweet)))
 
-    # Graph the results from the prob_of_new_tweets function
-    graph(prob_of_new_tweets(posTweets, wordsPos, wordsNeg),
-          prob_of_new_tweets(posTweetsCleanUp, wordsPos, wordsNeg),
-          (100 - prob_of_new_tweets(negTweets, wordsPos, wordsNeg)),
-          (100 - prob_of_new_tweets(negTweetsCleanUp, wordsPos, wordsNeg)))
+    print("Calculating Pre results...")
+    posPreResults = posNegTweets(posTweets, wordsPos, wordsNeg)
+    negPreResults = posNegTweets(negTweets, wordsPos, wordsNeg)
+    print("Pre Results\nPositive: ", posPreResults, "\nNegative: ", negPreResults)
 
+    print("Calculating Post results...")
+    posPostResults = posNegTweets(posTweetsCleanedUp, wordsPos, wordsNeg)
+    negPostResults = posNegTweets(negTweetsCleanedUp, wordsPos, wordsNeg)
+    print("Post Results\nPositive: ", posPostResults, "\nNegative: ", negPostResults)
+
+    graph(posPreResults[0], posPostResults[0], negPreResults[1], negPostResults[1])
 
 if __name__ == '__main__':
     main()
